6c88ef30cfb647e4ce9341c268c37ffbb2c91142
average-brokie <trevor.danese@gmail.com>
2025-05-02 21:31:52 -0400

    Initial commit

---
+++ repository.bat
@@ -0,0 +1,8 @@
+echo "# my-website" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/average-broke/my-website.git
+git push -u origin main
+pause
